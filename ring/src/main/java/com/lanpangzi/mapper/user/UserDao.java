09d050377deb3d98937b3865b10dedd019f109c0
myword <a1106410836@qq.com>
2018-12-20 15:02:23 +0800

    springboot +shiro +redis +oss+quartz

--- ring/src/main/java/com/lanpangzi/mapper/user/UserDao.java
+++ ring/src/main/java/com/lanpangzi/mapper/user/UserDao.java
@@ -1,10 +1,7 @@
 package com.lanpangzi.mapper.user;
 
 import com.lanpangzi.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
-@Mapper
 public interface UserDao {
     User getUserDao(Integer uid);
 }
